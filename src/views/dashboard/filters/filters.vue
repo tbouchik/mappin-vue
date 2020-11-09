b1b6678e2fe42c555b579fcc682a0b4c23dc7107
Taoufik <tbouchikhi1@hotmail.com>
2020-11-09 23:37:44 +0100

    button should float left hack

--- src/views/dashboard/filters/filters.vue
+++ src/views/dashboard/filters/filters.vue
@@ -3,7 +3,7 @@
     <br>
     <div class="air__utils__heading">
       <b-row>
-        <b-col md="3" class="my-1">
+        <b-col md="1" class="my-1">
           <h5>Templates </h5>
         </b-col>
         <b-col md="6" class="my-1">
