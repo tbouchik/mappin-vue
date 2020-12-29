f8f038086a67de9cdfbafcda19b35d67b9c90269
Taoufik <tbouchikhi1@hotmail.com>
2020-12-29 03:00:59 +0000

    versioning

--- src/components/layout/TopBar/Status/index.vue
+++ src/components/layout/TopBar/Status/index.vue
@@ -2,7 +2,7 @@
   <div>
     <span
       class="ml-2 p-1 badge bg-danger text-white font-size-12 text-uppercase"
-    >Trial Version</span>
+    >BETA Version</span>
   </div>
 </template>
 <script>
